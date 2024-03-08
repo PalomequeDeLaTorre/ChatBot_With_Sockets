79ab1add98d5affdfb633cc62daed27d25fb71fd
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-03-08 17:01:46 -0600

    chatBot_With_Socket.io

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,13 @@
 const socket = io();
-//console.log(socket);
-/*socket.on("saludo",(saludo)=>{
-    console.log("Estoy recibiendo tu mensaje "+ saludo);
-});
-
-socket.emit("respuesta","respuesta del cliente");*/
-
-//http://localhost:3000/socket.io/socket.io.min.js
 
 var enviarDatos = document.getElementById("enviarDatos");
 enviarDatos.addEventListener("submit", (e)=>{
     e.preventDefault();
-    var nombre=document.getElementById("nombre").value;
-    var datos=document.getElementById("datos");
-    socket.emit("nombre", nombre);
-    socket.on("saludo", (saludo)=>{
-        //console.log(saludo);
-        datos.innerHTML = saludo;
-    })
-
-    /*var usuario=document.getElementById("usuario").value;
-    var password=document.getElementById("password").value;
-    console.log("Formulario enviado.............");
-    console.log("nombre");
-    console.log("usuario");
-    console.log("password");
-    document.getElementById("nombre").value="";
-    document.getElementById("usuario").value="";
-    document.getElementById("password").value="";
-    document.getElementById("nombre").focus();*/
+    var mensaje = document.getElementById("mensaje").value;
+    var datos = document.getElementById("datos");
+    socket.emit("mensaje", mensaje);
+    socket.on("respuesta", (respuesta)=>{
+        datos.innerHTML = respuesta;
+    });
+    document.getElementById("mensaje").value = "";
 });
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,38 @@
-function socket(io){ //io es de input , output
+function socket(io){ 
     io.on("connection",(socket)=>{
-        io.emit("saludo","Hola soy el servidor"); //para servidor
-        socket.on("nombre",(nombre)=>{
-            //console.log("Hola "+nombre);
-            var saludo="Hola " + nombre;
-            io.emit("saludo", saludo);
+        io.emit("saludo","Hola soy el servidor");
+        let diaCita;
+        socket.on("mensaje",(mensaje)=>{
+            var respuesta;
+            switch (mensaje.toLowerCase()) {
+                case "hola":
+                    respuesta = "Hola, ¿cómo estás? ¿En qué puedo ayudarte?";
+                    break;
+                case "necesito agendar una cita":
+                    respuesta = "Claro, ¿qué día te gustaría agendar? Lunes, Martes, Miércoles, Jueves o Viernes?";
+                    break;
+                case "lunes":
+                case "martes":
+                case "miércoles":
+                case "jueves":
+                case "viernes":
+                    diaCita = mensaje;
+                    respuesta = `Por supuesto, ¿a qué hora? 9, 10, 11 o 12?`;
+                    break;
+                case "9":
+                case "10":
+                case "11":
+                case "12":
+                    respuesta = `Tu cita ha sido agendada con éxito para el día ${diaCita} a las ${mensaje}. ¿Hay algo más en lo que pueda ayudarte?`;
+                    break;
+                case "no":
+                    respuesta = "Muy bien, fue un placer ayudarte. ¡Que tengas un buen día!";
+                    break;
+                default:
+                    respuesta = "Lo siento, no entendí eso. ¿Puedes repetirlo?";
+            }
+            io.emit("respuesta", respuesta);
         });
     });
 }
-module.exports=socket;
+module.exports = socket;
